28c8080fee7d82897a9dac3972f76ab6c36dc558
pabobernando <pabobernando69@gmail.com>
2022-02-09 01:39:08 +0700

    first commit

---
+++ components/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1 className='font-bold text-3xl text-blue-400 text-center'>Quiz App</h1>
+    </div>
+  )
+}
