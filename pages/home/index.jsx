5b76d3211c458f1d481a9466d8e7d9b9c753cf6d
pabobernando <pabobernando69@gmail.com>
2022-02-13 21:03:11 +0700

    landing

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,13 +1,21 @@
 import Navbar from '@/components/Navbar/Navbar';
 import gambar from '../../img/stikps.jpg';
+import stickps from '../../img/stick.jpg';
 
 function HomePage() {
     return <div>
       <Navbar />
-      <div className='container mx-auto'>
-        <h1 className='text-6xl font-bold text-center mt-10'> Games Binar Accademy </h1>
+      <div className='container-fluid mx-auto  mt-9'>
+        <h1 className='text-6xl font-bold text-center'> Games Binar Accademy </h1>
         <p className='text-center text-4xl'>- Innovation in the game industry -</p>
-        <img src={gambar} />
+        <div className='flex items-center justify-center'>
+        <img className='object-center mt-9' src={stickps.src} />
+        </div>
+        <div className='text-center mt-8'>
+        <button class="bg-transparent hover:bg-violet-500 text-violet-700 font-semibold hover:text-white py-2 px-4 border border-violet-500 hover:border-transparent rounded">
+            Launch App
+        </button>
+        </div>
       </div>
     </div>
   }
